71a908ba6444ac77460a71346213ca0c14bd83cd
Jacky Lam <68130525+JackyNLam@users.noreply.github.com>
2022-03-24 21:58:49 +0800

    Update WorkTask.py

--- WorkTask.py
+++ WorkTask.py
@@ -14,25 +14,25 @@ class TaskWindow:
         self.master = master
         self.FirstFrame = Frame(master)
         master.title("Task")
-        master.geometry('950x390')
+        master.geometry('980x600')
         Label(self.FirstFrame, text="Status").grid(row=1,column=1)
         #Set up search box at the top
         self.tkvar = StringVar(master)
         self.myOption = ttk.Combobox(self.FirstFrame, width = 20, textvariable = self.tkvar)
         self.myOption.grid(row = 1, column = 2)
-        self.myOption['values'] = ["Pending","Completed"]
+        self.myOption['values'] = ["Pending","Completed","ToDo"]
         self.myOption.insert(0,"Pending")
-        self.my_tree = ttk.Treeview(self.FirstFrame)
-        self.my_tree.grid(row=2,column=1,rowspan = 6,columnspan=4)
+        self.my_tree = ttk.Treeview(self.FirstFrame,height=20)
+        self.my_tree.grid(row=2,column=1,rowspan = 8,columnspan=4)
         #set up columns for the tree view
         self.my_tree['columns'] = ('OrderID','Start_Time','End_Time','Description', 'Est_Time' , 'Actual_Time', 'Status')
         self.my_tree.column("#0",width=0,stretch="NO")
         self.my_tree.column("OrderID",width=0,stretch="NO")
-        self.my_tree.column("Start_Time",width=200)
-        self.my_tree.column("End_Time",width=200)
+        self.my_tree.column("Start_Time",width=145)
+        self.my_tree.column("End_Time",width=145)
         self.my_tree.column("Description",width=200)
-        self.my_tree.column("Est_Time",width=120)
-        self.my_tree.column("Actual_Time",width=120)
+        self.my_tree.column("Est_Time",width=80)
+        self.my_tree.column("Actual_Time",width=80)
         self.my_tree.column("Status",width=80)
         #set up column Name
         self.my_tree.heading('Start_Time', text='Start_Time', anchor='center')
@@ -45,18 +45,19 @@ class TaskWindow:
         self.refresh_task_list("Pending")
         #Add buttons on the top (customer search)
         Button(self.FirstFrame, text="Filter", width=20, command=self.filter_by_status).grid(row = 1, column =3,padx=30)
+        Button(self.FirstFrame, text="Refresh Time", width=20, command=self.RefreshStartTime).grid(row = 1,column = 4,padx=30)
         #Add buttons on the right
         Button(self.FirstFrame, text="Add Task", width=20, command=self.add_task).grid(row = 4, column =5,padx=30)
         Button(self.FirstFrame, text="Delete Task", width=20, command=self.delete_task).grid(row = 5, column =5,padx=30)
         Button(self.FirstFrame, text="Notes", width=20, command=self.Task_Note).grid(row = 6,column =5,padx=30)
         Button(self.FirstFrame, text="Complete", width=20, command=self.mark_as_complete).grid(row = 7,column = 5,padx=30)
         #Add buttons at the bottom
-        Button(self.FirstFrame, text="Start", width=20, command=self.start_working).grid(row = 8,column =2,padx=0,pady=30)
-        Button(self.FirstFrame, text="Pause", width=20, command=self.pause_working).grid(row = 8,column =3,padx=0,pady=30)
-        Button(self.FirstFrame, text="TimeTable", width=20, command=self.Show_TimeTable).grid(row = 8,column =4 ,padx=0,pady=30)
+        Button(self.FirstFrame, text="Start", width=20, command=self.start_working).grid(row = 10,column =2,padx=0,pady=30)
+        Button(self.FirstFrame, text="Pause", width=20, command=self.pause_working).grid(row = 10,column =3,padx=0,pady=30)
+        Button(self.FirstFrame, text="TimeTable", width=20, command=self.Show_TimeTable).grid(row = 10,column =4 ,padx=0,pady=30)
         #Add timer info at the bottom
         self.show_lapse_time = Label(self.FirstFrame, text="")
-        self.show_lapse_time.grid(row=9,column=2)
+        self.show_lapse_time.grid(row=11,column=2)
         #double click event
         self.my_tree.bind('<Double-Button-1>',self.viewclick)
         self.FirstFrame.pack(fill='both', expand=True)
@@ -86,18 +87,31 @@ class TaskWindow:
         self.task_note_window = Toplevel(self.master)
         self.app = NoteWindow(self.task_note_window)
     
-    def mark_as_complete(self):
+    def mark_as_complete(self):#Pending,Completed,ToDo
         x = self.my_tree.selection()[0]
         selected = self.my_tree.focus()
-        if self.my_tree.set(selected,'Status')!="Completed":
+        if self.my_tree.set(selected,'Status') == "Pending":
             cur.execute("UPDATE MyTask SET Status='Completed' WHERE TaskID={}".format(x))
             con.commit()
             self.my_tree.set(selected,'Status',"Completed")
-        else:
+        elif self.my_tree.set(selected,'Status') == "Completed":
+            cur.execute("UPDATE MyTask SET Status='ToDo' WHERE TaskID={}".format(x))
+            con.commit()
+            self.my_tree.set(selected,'Status',"ToDo")
+        elif self.my_tree.set(selected,'Status') == "ToDo":
             cur.execute("UPDATE MyTask SET Status='Pending' WHERE TaskID={}".format(x))
             con.commit()
             self.my_tree.set(selected,'Status',"Pending")
     
+    def RefreshStartTime(self):
+        for rowid in self.my_tree.selection():
+            self.Est_Time = self.my_tree.set(rowid,"Est_Time")
+            self.Act_Time = self.my_tree.set(rowid,"Actual_Time")
+            cur.execute("UPDATE MyTask SET Start_Time=datetime('now','localtime'),End_Time=datetime('now','+{0} minutes') WHERE TaskID={1}"\
+             .format(float(self.Est_Time)-float(self.Act_Time),rowid))
+            con.commit()
+            app.refresh_task_list("Pending")
+
     def filter_by_status(self):
         show_status = self.tkvar.get()
         if show_status == "" :
@@ -114,10 +128,9 @@ class TaskWindow:
         root.after_cancel(self.running_timer)
         self.show_lapse_time['text'] = ""
         duration = (time.time() - self.start_time)/60
-        prior_duration = cur.execute("SELECT Actual_Time from MyTask WHERE TaskID={}".format(self.working_item)).fetchone()[0]
-        duration = round(float(prior_duration)+duration,2)
-        cur.execute("UPDATE MyTask SET Actual_Time={} WHERE TaskID={}".format(duration,self.working_item))
         cur.execute("INSERT INTO TimeTrack(TaskID, Start_Time, End_Time) VALUES ({},'{}',datetime('now','localtime'))".format(self.working_item,datetime.datetime.fromtimestamp(self.start_time).strftime('%Y-%m-%d %H:%M:%S')))
+        duration = round(float(cur.execute("SELECT SUM(JULIANDAY(TimeTrack.End_Time) - JULIANDAY(TimeTrack.Start_Time)) FROM TimeTrack GROUP BY TimeTrack.TaskID HAVING TimeTrack.TaskID={}".format(self.working_item)).fetchone()[0]*24*60),2)
+        cur.execute("UPDATE MyTask SET Actual_Time={} WHERE TaskID={}".format(duration,self.working_item))
         con.commit()
         del self.working_item,self.start_time
         self.refresh_task_list("Pending")
@@ -184,9 +197,10 @@ class AmendTask:#for changing task detail
         Button(self.ThrFrame, text="Confirm", width=20, command=self.confirm_task_update).grid(row=5,column=2,pady=20)
         self.ThrFrame.pack()
     def confirm_task_update(self):
+        self.Act_Time = cur.execute("SELECT Actual_Time FROM MyTask WHERE TaskID ={}".format(app.my_tree.selection()[0])).fetchone()[0]
         cur.execute("UPDATE MyTask SET Start_Time='{0}',Description='{1}',\
-                    Est_Time={2},End_Time=datetime('{0}','+{2} minutes') WHERE TaskID={3}"\
-         .format(self.StartTime.get(),self.Description.get(),self.Est_Time.get(),app.my_tree.selection()[0]))
+                    Est_Time={2},End_Time=datetime('{0}','+{3} minutes') WHERE TaskID={4}"\
+         .format(self.StartTime.get(),self.Description.get(),self.Est_Time.get(),float(self.Est_Time.get())-self.Act_Time,app.my_tree.selection()[0]))
         con.commit()
         app.refresh_task_list("Pending")
         self.master.destroy()
@@ -197,18 +211,17 @@ class NoteWindow: #Show notes for the task
         self.master = master
         self.AddressFrame = Frame(master)
         master.title("Notes")
-        master.geometry('800x700')
-        Label(self.AddressFrame, text="Notes").grid(row=1,column=1)
+        master.geometry('800x450')
         self.Address_Info = cur.execute("SELECT Notes from MyTask WHERE TaskID = {}"\
                                         .format(app.my_tree.selection()[0])).fetchall()[0]
         if len(self.Address_Info)>0:
             self.Address_Info = str(self.Address_Info[0])
         else:
             self.Address_Info = ""
-        self.Address_Entry_Box = Text(self.AddressFrame,width=85)
+        self.Address_Entry_Box = Text(self.AddressFrame,width=85,height=20)
         self.Address_Entry_Box.insert(1.0,self.Address_Info)
-        self.Address_Entry_Box.grid(row=2,column=1,padx=50,ipady=70)
-        Button(self.AddressFrame, text="Save", width=20, command=self.Note_Confirm).grid(row = 3,column =1)
+        self.Address_Entry_Box.grid(row=1,column=1,padx=50,ipady=0)
+        Button(self.AddressFrame, text="Save", width=20, command=self.Note_Confirm).grid(row = 2,column =1)
         self.AddressFrame.pack(fill='both', expand=True)
 
     def Note_Confirm(self):
